e0c56279967f3681bdee9dbc3caef48095c61054
Yu Inoue <34591767+eternity1984@users.noreply.github.com>
2020-08-19 03:32:15 +0900

    :art: Add colormap support to plot_network_clusters
    Replace default COLOR with Matplotlib colormap
    Determine font color based on relative luminance
    Add colormap arg to plot_network_cluster and plot_community_graph

--- cdlib/viz/networks.py
+++ cdlib/viz/networks.py
@@ -1,3 +1,5 @@
+import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 import networkx as nx
 from cdlib import NodeClustering
@@ -6,12 +8,29 @@ from community import induced_graph
 
 __all__ = ["plot_network_clusters", "plot_community_graph"]
 
-COLOR = ['r', 'b', 'g', 'c', 'm', 'y', 'k',
-         '0.8', '0.2', '0.6', '0.4', '0.7', '0.3', '0.9', '0.1', '0.5']
+# [r, b, g, c, m, y, k, 0.8, 0.2, 0.6, 0.4, 0.7, 0.3, 0.9, 0.1, 0.5]
+COLOR = (
+    (1, 0, 0),
+    (0, 0, 1),
+    (0, 0.5, 0),
+    (0, 0.75, 0.75),
+    (0.75, 0, 0.75),
+    (0.75, 0.75, 0),
+    (0, 0, 0),
+    (0.8, 0.8, 0.8),
+    (0.2, 0.2, 0.2),
+    (0.6, 0.6, 0.6),
+    (0.4, 0.4, 0.4),
+    (0.7, 0.7, 0.7),
+    (0.3, 0.3, 0.3),
+    (0.9, 0.9, 0.9),
+    (0.1, 0.1, 0.1),
+    (0.5, 0.5, 0.5)
+)
 
 
 def plot_network_clusters(graph, partition, position=None, figsize=(8, 8), node_size=200, plot_overlaps=False,
-                          plot_labels=False):
+                          plot_labels=False, cmap=None):
     """
     Plot a graph with node color coding for communities.
 
@@ -22,6 +41,7 @@ def plot_network_clusters(graph, partition, position=None, figsize=(8, 8), node_
     :param node_size: int, default 200
     :param plot_overlaps: bool, default False. Flag to control if multiple algorithms memberships are plotted.
     :param plot_labels: bool, default False. Flag to control if node labels are plotted.
+    :param cmap: str or Matplotlib colormap, Colormap(Matplotlib colormap) for mapping intensities of nodes. If set to None, original colormap is used.
 
     Example:
 
@@ -32,12 +52,25 @@ def plot_network_clusters(graph, partition, position=None, figsize=(8, 8), node_
     >>> pos = nx.spring_layout(g)
     >>> viz.plot_network_clusters(g, coms, pos)
     """                  
+    if not isinstance(cmap, (type(None), str, matplotlib.colors.Colormap)):
+        raise TypeError("The 'cmap' argument must be NoneType, str or matplotlib.colors.Colormap, "
+                        "not %s." % (type(cmap).__name__))
+
     partition = partition.communities
     graph = convert_graph_formats(graph, nx.Graph)
     if position==None:
         position=nx.spring_layout(graph)
 
-    n_communities = min(len(partition), len(COLOR))
+    n_communities = len(partition)
+    if cmap is None:
+        n_communities = min(n_communities, len(COLOR))
+        cmap = matplotlib.colors.ListedColormap(COLOR[:n_communities])
+    else:
+        cmap = plt.cm.get_cmap(cmap, n_communities)
+    _norm = matplotlib.colors.Normalize(vmin=0, vmax=n_communities-1)
+    fontcolors = list(map(lambda rgb: ".15" if np.dot(rgb, [0.2126, 0.7152, 0.0722]) > .408 else "w",
+                          [cmap(_norm(i))[:3] for i in range(n_communities)]))
+
     plt.figure(figsize=figsize)
     plt.axis('off')
 
@@ -51,15 +84,19 @@ def plot_network_clusters(graph, partition, position=None, figsize=(8, 8), node_
             else:
                 size = node_size
             fig = nx.draw_networkx_nodes(graph, position, node_size=size,
-                                         nodelist=partition[i], node_color=COLOR[i])
+                                         nodelist=partition[i], node_color=[i] * len(partition[i]), 
+                                         cmap=cmap, vmin=0, vmax=n_communities-1)
             fig.set_edgecolor('k')
     if plot_labels:
-        nx.draw_networkx_labels(graph, position, labels={node: str(node) for node in graph.nodes()})
+        for i in range(n_communities):
+            if len(partition[i]) > 0:
+                nx.draw_networkx_labels(graph, position, font_color=fontcolors[i],
+                                        labels={node: str(node) for node in partition[i]})
 
     return fig
 
 
-def plot_community_graph(graph, partition, figsize=(8, 8), node_size=200, plot_overlaps=False, plot_labels=False):
+def plot_community_graph(graph, partition, figsize=(8, 8), node_size=200, plot_overlaps=False, plot_labels=False, cmap=None):
     """
         Plot a algorithms-graph with node color coding for communities.
 
@@ -69,6 +106,7 @@ def plot_community_graph(graph, partition, figsize=(8, 8), node_size=200, plot_o
         :param node_size: int, default 200
         :param plot_overlaps: bool, default False. Flag to control if multiple algorithms memberships are plotted.
         :param plot_labels: bool, default False. Flag to control if node labels are plotted.
+        :param cmap: str or Matplotlib colormap, Colormap(Matplotlib colormap) for mapping intensities of nodes. If set to None, original colormap is used..
 
         Example:
 
@@ -101,7 +139,7 @@ def plot_community_graph(graph, partition, figsize=(8, 8), node_size=200, plot_o
     node_cms = [[node] for node in c_graph.nodes()]
 
     return plot_network_clusters(c_graph, NodeClustering(node_cms, None, ""), nx.spring_layout(c_graph), figsize=figsize,
-                                 node_size=node_size, plot_overlaps=plot_overlaps, plot_labels=plot_labels)
+                                 node_size=node_size, plot_overlaps=plot_overlaps, plot_labels=plot_labels, cmap=cmap)
 
 
 
